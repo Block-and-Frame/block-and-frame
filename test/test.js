7fcc9a7c2089deff0e77102ffef105549259fe2b
Alex Ko <alexko@utexas.edu>
2016-03-12 16:54:44 -0700

    testing travis with passing mocha/chai tets

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ var expect = Chai.expect;
 describe('Testing Travis CI', function () {
 
   it('true === true', function () {
-    expect(true).to.equal(false);
+    expect(true).to.equal(true);
   });
 
 });
