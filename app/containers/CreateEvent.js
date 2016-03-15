7f5e5a06874bdaf2b7cbec5f9846e29d15d7d9c6
Alex Ko <alexko@utexas.edu>
2016-03-15 13:13:04 -0600

    created basic form for making event

--- app/containers/CreateEvent.js
+++ app/containers/CreateEvent.js
@@ -1,10 +1,82 @@
-import React from 'react';
+import React, { Component } from 'react';
+// import axios from 'axios';
 
-class CreateEvent extends React.Component {
+class CreateEvent extends Component {
+  constructor(props) {
+    super(props);
+
+    // TODO: add coordinates
+    this.state = {
+      name: '',
+      location: '',
+      description: '',
+    };
+
+    this.onNameChange = this.onNameChange.bind(this);
+    this.onLocationChange = this.onLocationChange.bind(this);
+    this.onDescriptionChange = this.onDescriptionChange.bind(this);
+    this.onEventSubmit = this.onEventSubmit.bind(this);
+    this.preventDefaultSubmit = this.preventDefaultSubmit.bind(this);
+  }
+
+  onNameChange(e) {
+    this.setState({ name: e.target.value });
+  }
+
+  onLocationChange(e) {
+    this.setState({ location: e.target.value });
+  }
+
+  onDescriptionChange(e) {
+    this.setState({ description: e.target.value });
+  }
+
+  onEventSubmit() {
+    const event = {
+      name: this.state.name,
+      location: this.state.location,
+      description: this.state.description,
+    };
+    // TODO: use axios to send event ot back end and reroute?
+
+    console.log(event);
+    this.setState({
+      name: '',
+      location: '',
+      description: '',
+    });
+  }
+
+  preventDefaultSubmit(e) {
+    e.preventDefault();
+  }
+
   render() {
     return (
       <div>
         CreateEvent hhere
+        <form
+          onSubmit={this.preventDefaultSubmit}
+        >
+          <label>Event Name: </label>
+          <input
+            value={this.state.name}
+            onChange={this.onNameChange}
+          /><br />
+          <label>Location: </label>
+          <input
+            value={this.state.location}
+            onChange={this.onLocationChange}
+          /><br />
+          <label>Description: </label>
+          <input
+            value={this.state.description}
+            onChange={this.onDescriptionChange}
+          /><br />
+          <button
+            onClick={this.onEventSubmit}
+          >Create!</button>
+        </form>
       </div>
     );
   }
