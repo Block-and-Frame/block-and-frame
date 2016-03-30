ece92ce1ca280f6551ed6d3435585e53300ab8a5
JT Yim <JT@JTYim.com>
2016-03-30 13:21:21 -0400

    File org/cleanup, pre-rebase

---
+++ app/components/landing/SplashImage.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class SplashImage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+  render() {
+    return (
+      <div className="splash">
+          <img id="1" src="" />
+          <img id="2" src="" />
+          <img id="3" src="" />
+          <img id="4" src="" />
+          <img id="5" src="" />
+      </div>
+    );
+  }
+}
+
+export default SplashImage;
--- app/components/users/UserEventListItem.js
+++ app/components/users/UserEventListItem.js
@@ -1,3 +1,4 @@
+<<<<<<< 319c0f0b75dfcd0f1c7a35e865af352656553a6b
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import moment from 'moment';
@@ -32,3 +33,36 @@ class UserEventListItem extends Component {
 }
 
 export default UserEventListItem;
+=======
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import moment from 'moment';
+
+class UserEventListItem extends Component {
+  render() {
+    const dateTime = moment(this.props.date)
+    .set({
+      hour: this.props.time.split(':')[0],
+      minute: this.props.time.split(':')[1],
+    })
+    .add(1, 'day'); // not sure why a day has to be added
+    
+    let status = this.props.isHost ? 'host' : 'attend';
+    status = moment() > dateTime ? `${status}ed` : `will ${status}`;
+    
+    return (
+      <div className="item">
+        <div className="content">
+          <Link className="header" to={`/${this.props.id}`}>
+            {status} {this.props.name}
+          </Link>
+          <div>{dateTime.fromNow()}</div>
+          <div>{this.props.location}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default UserEventListItem;
+>>>>>>> File org/cleanup, pre-rebase
